50a9e836628f0a4f633526e6e7f3314798678037
Shreevara Andila <sandila1@cs.binghamton.edu>
2022-09-24 21:43:34 -0400

    Assigning courses

--- studentCoursesBackup/src/studentCoursesBackup/driver/Driver.java
+++ studentCoursesBackup/src/studentCoursesBackup/driver/Driver.java
@@ -2,20 +2,17 @@ package studentCoursesBackup.driver;
 import studentCoursesBackup.util.FileDisplayInterface;
 import studentCoursesBackup.util.FileProcessor;
 
-
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Course {
   private int capacity;
-  private int classTime;
+  private String classTime;
   private char courseName;
-
-  public Course(int capacity, int classTime, char courseName) {
+  public Course(int capacity, String classTime, char courseName) {
     this.capacity = capacity;
     this.classTime = classTime;
     this.courseName = courseName;
@@ -25,7 +22,8 @@ class Course {
     return capacity;
   }
 
-  public int getClassTime() {
+
+  public String getClassTime() {
     return classTime;
   }
 
@@ -36,9 +34,11 @@ class Course {
   public void setCapacity(int capacity) {
     this.capacity = capacity;
   }
+
+
   @Override
   public String toString() {
-    return "CourseRegistration{" +
+    return "Course{" +
                 "capacity=" + capacity +
                 ", classTimings=" + classTime +
                 ", courseName='" + courseName +
@@ -50,13 +50,15 @@ class Course {
 class Student {
   private int id;
   private char[] preferences;
-  private char[] courses;
+  private ArrayList<String> my_courses;
+  private ArrayList<String> my_timings;
   private int satisfaction;
 
   public Student(int id, char[] preferences) {
     this.id = id;
     this.preferences = preferences;
-    this.courses = new char[3];
+    this.my_courses = new ArrayList<String>();
+    this.my_timings = new ArrayList<String>();
     this.satisfaction = 0;
   }
 
@@ -68,8 +70,12 @@ class Student {
     return preferences;
   }
 
-  public char[] getCourses() {
-    return courses;
+  public ArrayList<String> getCourses() {
+    return my_courses;
+  }
+
+  public ArrayList<String> getTimings() {
+    return my_timings;
   }
 
   public int getSatisfaction() {
@@ -80,18 +86,25 @@ class Student {
     this.preferences = preferences;
   }
 
-  public void setCourses(char[] courses) {
-    this.courses = courses;
+  public void setCourses(ArrayList<String> courses) {
+    this.my_courses = courses;
+  }
+
+  public void setTimings(ArrayList<String> time) {
+    this.my_timings = time;
   }
 
   public void setSatisfaction(int satisfaction) {
     this.satisfaction = satisfaction;
   }
+  
+  
   @Override
   public String toString() {
-    return "CoursePrefs{" +
+    return "student{" +
                 "id=" + id +
-                ", preferences=" + preferences +
+                ", pref=" + String.valueOf(preferences) +
+
                 '}';
   }
 }
@@ -103,19 +116,19 @@ public class Driver {
   public static void main(String[] args) {
     readCourseInfo();
     readCoursePrefs();
-
+    assignCourses();
   }
 
   private static void readCourseInfo() {
     courses = new Course[9];
     try {
-      Scanner s = new Scanner(new File("/home/sandila1/DP/cs542-fall-22-assign1-shreevaraandila1/studentCoursesBackup/src/studentCoursesBackup/courseInfo.txt"));
+      Scanner s = new Scanner(new File("/home/sandila1/DP/cs542-fall-22-assign1-shreevaraandila1/studentCoursesBackup/src/studentCoursesBackup/driver/courseInfo.txt"));
       for (int i = 0; i < 9; i++) {
         String line = s.nextLine();
         String[] parts = line.split(":");
         char courseName = parts[0].charAt(0);
         int capacity = Integer.parseInt(parts[1]);
-        int classTime = Integer.parseInt(parts[2]);
+        String classTime = parts[2];
         courses[i] = new Course(capacity, classTime, courseName);
       }
       s.close();
@@ -127,28 +140,64 @@ public class Driver {
   }
 
   private static void readCoursePrefs() {
-    students = new Student[1000];
+    students = new Student[10];
     try {
-      Scanner s = new Scanner(new File("/home/sandila1/DP/cs542-fall-22-assign1-shreevaraandila1/studentCoursesBackup/src/studentCoursesBackup/coursePrefs.txt"));
+      Scanner s = new Scanner(new File("/home/sandila1/DP/cs542-fall-22-assign1-shreevaraandila1/studentCoursesBackup/src/studentCoursesBackup/driver/coursePrefs.txt"));
+      int j=0;
       while (s.hasNextLine()) {
         String line = s.nextLine();
         String[] parts = line.split(" ");
-        int id = Integer.
-        parseInt(parts[0]);
+        int id = Integer.parseInt(parts[0]);
         char[] preferences = new char[9];
         for (int i = 0; i < 9; i++) {
           preferences[i] = parts[i + 1].charAt(0);
         }
-        students[id] = new Student(id, preferences);
+        students[j] = new Student(id, preferences);
+        j=j+1;
       }
       s.close();
     } 
     catch (FileNotFoundException e) {
       e.printStackTrace();
     }
+    System.out.println(Arrays.toString(students));
   }
- 
 
+  public static void assignCourses() {
+    for(Student student : students){
+        if(student != null){
+            for(int i =0; i < 9; i++){
+                char coursename =  student.getPreferences()[i];
+                int satisfactionRating = 9 - i;
+                for(Course course : courses){
+                    if (course.getCourseName() == coursename){
+                        String time = course.getClassTime();
+                        int capacityleft = course.getCapacity();
+                        ArrayList<String> coursesreg = student.getCourses();
+                        if(capacityleft > 0 ){
+                            ArrayList<String> timings = student.getTimings();
+                            for(String mytime : timings){
+                                if(time==mytime){
+                                    System.out.println("Time conflict");
+                                }
+                                else{
+                                    timings.add(time);
+                                }
+                            }
+                        String s = Character.toString(coursename);
+                        coursesreg.add(s);
+                        }
+                    }
 
- 
+                }
+            }
+        }
+    }
+
+      
+    }
 }
+
+
+
+ 
